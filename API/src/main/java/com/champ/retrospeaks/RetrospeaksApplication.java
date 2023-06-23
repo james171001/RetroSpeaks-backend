ff9de90f2f186885073b6f65f7532aaa81156d58
James <jameserne9@gmail.com>
2023-06-23 14:18:27 +0800

    first commit

---
+++ API/src/main/java/com/champ/retrospeaks/RetrospeaksApplication.java
@@ -0,0 +1,13 @@
+package com.champ.retrospeaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospeaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrospeaksApplication.class, args);
+	}
+
+}
